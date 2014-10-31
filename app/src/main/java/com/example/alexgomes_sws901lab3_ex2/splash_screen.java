9330f8f8381138196b6a1f3d9d4e6345da1fe691
alexgomes09 <alex_09hg@yahoo.com>
2014-10-31 15:42:13 -0700

    Initial Release

---
+++ app/src/main/java/com/example/alexgomes_sws901lab3_ex2/splash_screen.java
@@ -0,0 +1,16 @@
+package com.example.alexgomes_sws901lab3_ex2;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class splash_screen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+    }
+}
