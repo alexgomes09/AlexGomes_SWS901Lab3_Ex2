a3eae389aa0ba9082fcf51a6e705ac1d181fa103
alexgomes09 <alex_09hg@yahoo.com>
2014-11-01 04:30:36 -0700

    map_data.xml added | map view working

--- app/src/main/java/com/example/alexgomes_sws901lab3_ex2/ListRestaurants.java
+++ app/src/main/java/com/example/alexgomes_sws901lab3_ex2/ListRestaurants.java
@@ -2,11 +2,17 @@ package com.example.alexgomes_sws901lab3_ex2;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * Created by Alex on 10/31/2014.
@@ -14,6 +20,7 @@ import android.widget.ListView;
 public class ListRestaurants extends Activity {
 
     ListView listView;
+    public ListAdapter listAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,13 +28,15 @@ public class ListRestaurants extends Activity {
 
         listView = (ListView)findViewById(R.id.listView);
 
-
+        listAdapter = new ListAdapter(this);
+        listView.setAdapter(listAdapter);
 
     }
     class ListAdapter extends BaseAdapter{
 
         Context context;
-        String[] mapData;
+        String[] mapData = null;
+        TextView addressName,addressLat,addressLong;
 
         public ListAdapter(Context c){
             context = c;
@@ -52,7 +61,16 @@ public class ListRestaurants extends Activity {
         @Override
         public View getView(int i, View view, ViewGroup viewGroup) {
 
-            
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.single_map_data,null);
+
+            addressName = (TextView)view.findViewById(R.id.addressName);
+            addressLat = (TextView)view.findViewById(R.id.addressLat);
+            addressLong = (TextView)view.findViewById(R.id.addressLong);
+
+            addressName.setText(mapData[i]);
+            addressLat.setText(mapData[i]);
+            addressLong.setText(mapData[i]);
 
             return view;
         }
